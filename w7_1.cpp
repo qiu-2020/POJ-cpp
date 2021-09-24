8244922a8c8921c69c17404171f4d4ab4922d29c
“qiu-2020” <qiuzy@hnu.edu.cn>
2021-09-24 15:18:54 +0800

    main

---
+++ w7_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    freopen("text.txt","w",stdout);
+    if(y == 0)
+        cerr <<"error"<<std::endl;
+    else
+        cout<<x/y<<std::endl;
+
+    return 0;
+}
