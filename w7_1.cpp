81a65f69aa26f5a3828906c808501fa6fea199c9
“qiu-2020” <qiuzy@hnu.edu.cn>
2021-09-24 15:42:08 +0800

    main

--- w7_1.cpp
+++ w7_1.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 
 int main(){
+    // int x,y;
+    // cin>>x>>y;
+    // freopen("text.txt","w",stdout);
+    // if(y == 0)
+    //     cerr <<"error"<<std::endl;
+    // else
+    //     cout<<x/y<<std::endl;
+
     int x,y;
+
+    freopen("text.txt","r",stdin);
     cin>>x>>y;
-    freopen("text.txt","w",stdout);
-    if(y == 0)
-        cerr <<"error"<<std::endl;
-    else
-        cout<<x/y<<std::endl;
+    std::cout<<x<<','<<y<<std::endl;
 
     return 0;
-}
+}   
